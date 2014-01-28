72422bcc889eb33b8554c6c592641e881d80b502
Jerry Sims <jerrysims07@gmail.com>
2014-01-28 14:45:50 -0600

    REFACTOR: staples class created

--- test/test_entering_items.rb
+++ test/test_entering_items.rb
@@ -1,15 +1,15 @@
 require 'minitest/autorun'
 
-class TestEnteringItems < Minitest::Unit::TestCase
+class TestEnteringStaples < Minitest::Unit::TestCase
 
   def test_no_item_name_on_command_line
     command = `./ptickle add`
-    assert_equal "You must provide the name of the product you are adding.\nYou must include target inventory amount when adding items.", command.strip
+    assert_equal "You must provide the name of the product.", command.strip
   end
 
   def test_no_target_inventory_amount_on_command_line
     command = `./ptickle add --name Cheerios`
-    assert_equal "You must include target inventory amount when adding items.", command.strip
+    assert_equal "You must include [\"target inventory amount\"] when adding items.", command.strip
   end
   
   def test_add_item_will_add_Item_to_database
